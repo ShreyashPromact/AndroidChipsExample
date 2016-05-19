8c46870fdcdf35200ca0bdf47da51a6e066283fb
Shreyash <Shreyash@SHREYASH.promact.com>
2016-05-19 18:42:32 +0530

    Added Screen shots

--- README.md
+++ README.md
@@ -3,6 +3,4 @@
 Its an example of creating chips without having autocompete EditText in it which shows contacts name. But here it is not showing any contacts name while typing. You can check it out and try it.
 
 
-http://imgur.com/yPYs5tU
 
-http://imgur.com/0Ug7Ygq
--- app/src/main/java/com/shreyash/chipsexample/ChipsScreen.java
+++ app/src/main/java/com/shreyash/chipsexample/ChipsScreen.java
@@ -8,11 +8,11 @@ import com.android.ex.chips.BaseRecipientAdapter;
 import com.android.ex.chips.RecipientEditTextView;
 
 public class ChipsScreen extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chips_screen);
+
         RecipientEditTextView mTagsET = (RecipientEditTextView) findViewById(R.id.email_retv);
         mTagsET.setTokenizer(new Rfc822Tokenizer());
         mTagsET.setAdapter(new BaseRecipientAdapter(BaseRecipientAdapter.QUERY_TYPE_EMAIL, this) {
